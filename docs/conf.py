bd622ce36f331892ac03a6c43ac3b345b19ab119
Robert Reischke <robert@Roberts-MBP.fritz.box>
2025-02-23 15:23:16 +0100

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 html_logo = 'levin_logo.jpeg'
 html_theme_options = {
     'logo_only': True,
